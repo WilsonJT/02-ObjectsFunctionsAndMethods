01e172768dd7cbeb3c23b5724c41cc39fdc1ef00
wilsonjt <wilsonjt@rose-hulman.edu>
2018-11-28 18:49:48 -0500

    m2 coming right up!

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Jack Wilson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -18,9 +18,12 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 ###############################################################################
 
 import rosegraphics as rg
-
+import math
 
 def main():
+    print(hypotenuse_finder(3, 4))
+    turtle_maker('Koopa', 'green', 10)
+    turtle_maker('Paratroopa', 'red', 3)
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
@@ -28,7 +31,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# DONE: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -36,10 +39,14 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE: 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
+def hypotenuse_finder(a, b):
+    d = a ** 2 + b ** 2
+    c = math.sqrt(d)
+    return c
 
 
 ###############################################################################
@@ -69,7 +76,12 @@ def main():
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
-
+def turtle_maker(name, color, thiccness):
+    name = rg.SimpleTurtle()
+    name.pen = rg.Pen(color, thiccness)
+    print('Name: ', name)
+    print('Color: ', color)
+    print('Thiccness: ', thiccness)
 
 ###############################################################################
 # TODO: 5.
